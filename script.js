307c3e4b271ec9993be925e7920e8848b5450bf1
Alexander Artamonov <alexander.y.artamonov@gmail.com>
2025-04-28 17:56:51 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -3,18 +3,9 @@ const registerLink = document.querySelector('.register-link');
 const loginLink = document.querySelector('.login-link');
 const popupButton = document.querySelector('.Login-popup-button');
 const iconClose = document.querySelector('.icon-close');
+let list = document.querySelectorAll('.list .item')
 
 document.addEventListener('DOMContentLoaded', () => {
-
-const testimonialSwiper = new Swiper('.feedback',{ 
-    speed: 400,
-    loop: true,
-    spaceBetween: 30,
-    navigation:{
-        nextEl: ".btn_next",
-        prevEl: ".btn_previous",
-    },
-});
 
 registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
@@ -31,4 +22,36 @@ popupButton.addEventListener('click', ()=> {
 iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
 });
+
+const testimonialSwiper = new Swiper('.feedback',{ 
+    speed: 400,
+    loop: true,
+    spaceBetween: 30,
+    navigation:{
+        nextEl: ".btn_next",
+        prevEl: ".btn_previous",
+    },
+});
+
+list.forEach(item => {
+    const dropdown = item.querySelector('.dropdown');
+    const text = item.querySelector('.text');
+    
+    text.addEventListener('click', function() {
+        if (item.classList.contains('active')) {
+            item.classList.remove('active');
+        } else {
+            item.classList.add('active');
+        }
+    });
+
+    dropdown.addEventListener('click', function(event) {
+    if (item.classList.contains('active')) {
+        item.classList.remove('active');
+    } else {
+        item.classList.add('active');
+    }
+});
+});
+
 });
